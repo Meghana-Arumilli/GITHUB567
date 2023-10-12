0501b0a0d578a1bcc26e2749d9d3b466565ca54d
Arumilli Meghana <meghanamaggi2020@gmail.com>
2023-10-11 22:36:46 -0400

    first commit

---
+++ github.py
@@ -0,0 +1,32 @@
+import requests, json
+
+def get_from_github(username):
+
+    url = 'https://api.github.com/users/' +  username + '/repos'    
+    data = requests.get(url)
+    result = []
+    # print(data.status_code)
+    if data.status_code == 200:
+        repos = json.loads(data.text)
+        for repo in repos:
+            repourl = repo['url']
+            repodata = requests.get(repourl)
+            if repodata.status_code == 200:
+                repodata = json.loads(repodata.text)
+                commiturl = 'https://api.github.com/repos/' + username + '/' + repodata['name'] + '/commits'
+                commitdata = requests.get(commiturl)
+                if commitdata.status_code == 200:
+                    commitdata = json.loads(commitdata.text)
+                    commits = len(commitdata)
+                    s = 'Repo: ' + repodata['name'] + ' Number of commits: ' + str(commits)
+                    print(s)
+                    result.append(s)
+            else:
+                return 'unable to fetch repos from user'
+        return result
+    else:
+        return 'unable to fetch repos from user'
+
+if __name__ == '__main__':
+    result = get_from_github('Meghana-Arumilli')
+    print(result)
